dc73221c0dba8e47df9f7a840548ea556f4cdb43
HudsonRAlves <hudsonralves@hotmail.com>
2021-03-09 19:05:21 -0300

    Teste Commit

--- src/main/java/br/com/hudson/transporte/TransporteApplication.java
+++ src/main/java/br/com/hudson/transporte/TransporteApplication.java
@@ -3,10 +3,6 @@ package br.com.hudson.transporte;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*Teste*/
-/*Teste*/
-/*Teste*/
-
 @SpringBootApplication
 public class TransporteApplication {
 
